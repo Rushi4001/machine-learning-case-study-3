22f3d67d49a5e66e905fbbcd65a661c76c1ce7f9
Rushikesh rajendra kale <75717564+Rushi4001@users.noreply.github.com>
2021-04-17 20:55:08 +0530

    Add files via upload
    • In this application we remove one entry from each label of iris dataset and train with 
    the remaining entries. 
    • And we apply predictions based on Decision tree with that removed entries

---
+++ iris3.py
@@ -0,0 +1,38 @@
+from sklearn.datasets import load_iris
+import numpy as np
+from sklearn import tree
+
+def main():
+    dataset=load_iris()
+    
+    print("features of dataset")
+    print(dataset.feature_names)
+
+    print("target names of dataset")
+    print(dataset.target_names)
+     
+    index=[1,51,101]
+    test_target=dataset.target[index]
+    test_feature=dataset.data[index]
+    
+    train_target=np.delete(dataset.target,index)
+    train_feture=np.delete(dataset.data,index,axis=0)
+    
+    obj=tree.DecisionTreeClassifier()
+    
+    obj.fit(train_feture,train_target)
+    
+    result=obj.predict(test_feature)
+    
+    print("result prediction by ML", result)
+    
+    print("result expected",test_target)
+    
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+
